0a698d0a235c01541d670f5b396874228b57c9b9
sergokiko <sergokiko@gmail.com>
2025-10-17 11:07:15 +0200

    CntrlSDK -> to CntrlComponents  in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "CntrlSDK",
+      name: "CntrlComponents",
       fileName: (format) => `index.${format}.js`,
       formats: ["es", "cjs"],
     },
