d3fb5c116c114e4bf17fd3bf77c294f75583ada6
Paula <paulet1207@gmail.com>
2025-11-18 17:30:09 +0100

    CNTRL-lightbox: added mode to 'settings-image-input'  for diff styling

--- src/Components/Lightbox/LightboxComponent.ts
+++ src/Components/Lightbox/LightboxComponent.ts
@@ -37,6 +37,7 @@ export const LightboxComponent: Component = {
                   url: {
                     type: 'string',
                     display: {
+                      mode: 'cover',
                       type: 'settings-image-input',
                     }
                   }
