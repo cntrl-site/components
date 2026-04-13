138c100c3f77c7484880d797a2f5c3c24d54a5a3
Paula <paulet1207@gmail.com>
2026-04-13 13:53:27 +0200

    CNTRL-2514: Refactor OnelinerForm and FormComponent to enhance styling and layout; update button settings and schema properties for improved functionality.

--- src/Components/Form/FormComponent.ts
+++ src/Components/Form/FormComponent.ts
@@ -61,26 +61,11 @@ const paletteBookmarkItems = [
   'errorColor',
 ] as const;
 
-const textStyleDefault = (fontWeight: number) => ({
-  fontSettings: {
-    fontWeight,
-    fontStyle: 'normal',
-  },
-  lineHeight: 0.01,
-  letterSpacing: 0,
-  wordSpacing: 0,
-  fontSize: 0.01,
-  textAppearance: {
-    textTransform: 'none',
-    textDecoration: 'none',
-    fontVariant: 'normal',
-  }
-});
 const schema: ComponentSchemaV1 = {
   type: 'object',
   version: 1,
   settings: {
-    sizing: 'auto', // TODO think where to place this non-editable property
+    sizing: 'auto manual', // TODO think where to place this non-editable property
     properties: {
       fieldsToShow: {
         type: 'number',
